b0d7351b02de7fa5c876394f329edea1f2598d6a
Robert Zetzsche <zetzsche@movisens.com>
2019-05-21 18:32:55 +0200

    added recyclerview and implemented adapter added onclick methods to start pairing added base handling of throwables made scan activity entry point

--- app/src/main/java/com/movisens/rxblemovisenssample/scan/ScanActivity.kt
+++ app/src/main/java/com/movisens/rxblemovisenssample/scan/ScanActivity.kt
@@ -1,18 +1,31 @@
 package com.movisens.rxblemovisenssample.scan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.movisens.rxblemovisenssample.R
 import com.movisens.rxblemovisenssample.model.MovisensDevice
 import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_scan.*
 
 class ScanActivity : AppCompatActivity() {
     lateinit var scanViewModel: ScanViewModel
     lateinit var scanDisposable: Disposable
+    lateinit var adapter: ScanRecyclerViewAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         scanViewModel = ViewModelProviders.of(this).get(ScanViewModel::class.java)
+        setContentView(R.layout.activity_scan)
+        adapter = ScanRecyclerViewAdapter {
+            Log.e("TEST", it.toString())
+        }
+        devices_recyclerview.layoutManager = LinearLayoutManager(this)
+        devices_recyclerview.adapter = adapter
     }
 
     override fun onResume() {
@@ -21,12 +34,13 @@ class ScanActivity : AppCompatActivity() {
             .subscribe({ device -> showDevice(device) }, { throwable -> showError(throwable) })
     }
 
-    private fun showError(throwable: Throwable?) {
-
+    private fun showError(throwable: Throwable) {
+        throwable.printStackTrace()
+        Snackbar.make(activity_scan_root, throwable.localizedMessage, Snackbar.LENGTH_LONG).show()
     }
 
-    private fun showDevice(device: MovisensDevice?) {
-
+    private fun showDevice(device: MovisensDevice) {
+        adapter.addDevice(device)
     }
 
     override fun onPause() {
---
+++ app/src/main/java/com/movisens/rxblemovisenssample/scan/ScanRecyclerViewAdapter.kt
@@ -0,0 +1,57 @@
+package com.movisens.rxblemovisenssample.scan
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.movisens.rxblemovisenssample.R
+import com.movisens.rxblemovisenssample.model.MovisensDevice
+import kotlinx.android.synthetic.main.card_ble_device.view.*
+
+/**
+ * Created by Robert Zetzsche on 21.05.2019.
+ */
+class ScanRecyclerViewAdapter(private val itemClick: (MovisensDevice) -> Unit) :
+    RecyclerView.Adapter<ScanResultViewHolder>() {
+
+    var movisensDeviceList: ArrayList<MovisensDevice> = arrayListOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    fun addDevice(device: MovisensDevice) {
+        for (movisensDevice in movisensDeviceList) {
+            if (movisensDevice.mac == device.mac) {
+                val index = movisensDeviceList.indexOf(movisensDevice)
+                movisensDeviceList.remove(movisensDevice)
+                movisensDeviceList.add(index, device)
+                notifyItemChanged(index)
+                return
+            }
+        }
+        movisensDeviceList.add(device)
+        notifyItemInserted(movisensDeviceList.size - 1);
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScanResultViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_ble_device, parent, false)
+        return ScanResultViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return movisensDeviceList.size
+    }
+
+    override fun onBindViewHolder(holder: ScanResultViewHolder, position: Int) {
+        val movisensDevice = movisensDeviceList[position]
+        holder.itemView.apply {
+            ble_name_text.text = movisensDevice.name
+            mac_name_text.text = movisensDevice.mac
+            rssi_name_text.text = movisensDevice.rssi.toString()
+            setOnClickListener { itemClick.invoke(movisensDevice) }
+        }
+    }
+}
+
+class ScanResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
