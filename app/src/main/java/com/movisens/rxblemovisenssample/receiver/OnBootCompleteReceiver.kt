f6f3b4fa32dd21d2a39e2ce2f9925d9374d5b6fd
Robert Zetzsche <zetzsche@movisens.com>
2019-05-27 16:58:17 +0200

    added OnBootCompelte implementation

--- app/src/main/java/com/movisens/rxblemovisenssample/receiver/OnBootCompleteReceiver.kt
+++ app/src/main/java/com/movisens/rxblemovisenssample/receiver/OnBootCompleteReceiver.kt
@@ -1,5 +1,15 @@
 package com.movisens.rxblemovisenssample.receiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
 /**
  * Created by Robert Zetzsche on 27.05.2019.
  */
+class OnBootCompleteReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        // Nothing to do here because boot complete intent will start
+        // application which handles the restart of the service
+    }
+}
