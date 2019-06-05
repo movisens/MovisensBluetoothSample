d79eb54d8866aa33fc28e04b81e006eacf952616
Robert Zetzsche <zetzsche@movisens.com>
2019-06-05 18:03:41 +0200

    deleted binder interface added code to sucessully connect to sensor

--- app/src/main/java/com/movisens/rxblemovisenssample/bluetooth/BluetoothService.kt
+++ app/src/main/java/com/movisens/rxblemovisenssample/bluetooth/BluetoothService.kt
@@ -1,25 +1,95 @@
 package com.movisens.rxblemovisenssample.bluetooth
 
-import android.app.Service
+import android.app.*
+import android.app.PendingIntent.*
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import com.movisens.rxblemovisenssample.bluetooth.binder.BluetoothBinder
-import com.movisens.rxblemovisenssample.bluetooth.binder.BluetoothBinderInterface
+import com.movisens.rxblemovisenssample.bluetooth.binder.IBluetoothBinder
+import com.movisens.rxblemovisenssample.exceptions.ReconnectException
+import com.movisens.rxblemovisenssample.exceptions.UnrecoverableException
+import com.movisens.rxblemovisenssample.feature.connect.ConnectActivity
+import com.movisens.rxblemovisenssample.model.MovisensDevicesRepository
+import com.polidea.rxandroidble2.RxBleClient
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Robert Zetzsche on 27.05.2019.
  */
 
 class BluetoothService : Service() {
+    private var reconnectPendingIntent: PendingIntent? = null
+    private lateinit var errorDisposable: Disposable
+    private lateinit var IBluetoothBinder: IBluetoothBinder
+    private lateinit var bluetoothController: BluetoothServiceController
+    private lateinit var movementAccelerationDisposable: Disposable
+    private val subject: Subject<Boolean> = PublishSubject.create()
+    private val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-    private lateinit var bluetoothBinder: BluetoothBinderInterface
+    companion object {
+        const val COMMAND = "COMMAND"
+        const val COMMAND_RECONNECT = "COMMAND_RECONNECT"
+        const val SENSOR_MAC = "SENSOR_MAC"
+    }
 
     override fun onBind(p0: Intent?): IBinder {
-        return bluetoothBinder
+        return IBluetoothBinder
     }
 
     override fun onCreate() {
         super.onCreate()
-        bluetoothBinder = BluetoothBinder()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null && intent.hasExtra(SENSOR_MAC) && intent.hasExtra(COMMAND)) {
+            val command: String = intent.getStringExtra(COMMAND)
+            val mac: String = intent.getStringExtra(SENSOR_MAC)
+            when (command) {
+                "START_COMMAND" -> {
+                    if (intent.hasExtra(SENSOR_MAC)) {
+                        showForegroundNotification()
+                        bluetoothController =
+                            BluetoothServiceController(MovisensDevicesRepository(RxBleClient.create(this)), mac)
+                        IBluetoothBinder = BluetoothBinder(bluetoothController)
+                        movementAccelerationDisposable = bluetoothController.getMovementAccObservable(subject)
+                            .subscribe()
+                        errorDisposable = bluetoothController.errorSubject.subscribe { handleErrors(it, mac) }
+                    }
+                }
+                "STOP_COMMAND" -> {
+                    bluetoothController.stopSensor().subscribe()
+                }
+                "RECONNECT" -> {
+                    subject.onNext(true)
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun showForegroundNotification() {
+        val notification: Notification? = null
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        startForeground(123, notification)
+    }
+
+    private fun handleErrors(throwable: Throwable, mac: String) {
+        if (throwable is ReconnectException) {
+            val serviceIntent = Intent(this, BluetoothService::class.java)
+            serviceIntent.putExtra(COMMAND, COMMAND_RECONNECT)
+            serviceIntent.putExtra(SENSOR_MAC, mac)
+            reconnectPendingIntent = getService(this, 45698, serviceIntent, FLAG_UPDATE_CURRENT)
+            val triggerAtMillis = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(5)
+            val intent = Intent(this, ConnectActivity::class.java)
+            val pi = getActivity(this, System.currentTimeMillis().toInt(), intent, FLAG_UPDATE_CURRENT)
+            val alarmClockInfo = AlarmManager.AlarmClockInfo(triggerAtMillis, pi)
+            alarmManager.setAlarmClock(alarmClockInfo, reconnectPendingIntent)
+        } else if (throwable is UnrecoverableException) {
+            // Do something
+        }
     }
 }
--- app/src/main/java/com/movisens/rxblemovisenssample/bluetooth/binder/BluetoothBinder.kt
+++ app/src/main/java/com/movisens/rxblemovisenssample/bluetooth/binder/BluetoothBinder.kt
@@ -1,17 +1,20 @@
 package com.movisens.rxblemovisenssample.bluetooth.binder
 
 import android.os.Binder
+import android.os.IBinder
 import io.reactivex.Observable
 
 /**
  * Created by Robert Zetzsche on 27.05.2019.
  */
-class BluetoothBinder : Binder(), BluetoothBinderInterface {
-    override fun stopSensor() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class BluetoothBinder(val bluetoothService: BluetoothContract.Controller) : Binder(), IBluetoothBinder {
+    override fun getMovementAccObservable(): Observable<Double> {
+        return bluetoothService.getMovementAccObservable()
     }
 
-    override fun getMovementAccObservable(): Observable<Double> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun stopSensor(): Observable<Boolean> {
+        return bluetoothService.stopSensor()
     }
 }
+
+interface IBluetoothBinder : IBinder, BluetoothContract.Controller
