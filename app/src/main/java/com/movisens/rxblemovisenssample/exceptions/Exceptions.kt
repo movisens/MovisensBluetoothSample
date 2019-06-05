b4157854f0b883a092a107be0ec5aea037cf7374
Robert Zetzsche <zetzsche@movisens.com>
2019-06-05 17:57:35 +0200

    added exceptions classes for unrecoverable error and recoverable errors

---
+++ app/src/main/java/com/movisens/rxblemovisenssample/exceptions/Exceptions.kt
@@ -0,0 +1,8 @@
+package com.movisens.rxblemovisenssample.exceptions
+
+/**
+ * Created by Robert Zetzsche on 28.05.2019.
+ */
+class ReconnectException : Exception()
+
+class UnrecoverableException : Exception()
