069dfa6012d2263b688a852ff0ebad60be74a7e0
Robert Zetzsche <zetzsche@movisens.com>
2019-06-06 18:15:12 +0200

    added right restart logic

--- app/src/main/java/com/movisens/rxblemovisenssample/application/Application.kt
+++ app/src/main/java/com/movisens/rxblemovisenssample/application/Application.kt
@@ -5,9 +5,15 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.content.Context
+import android.content.Intent
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.O
 import android.preference.PreferenceManager
+import androidx.core.content.edit
+import com.movisens.rxblemovisenssample.bluetooth.BluetoothService
+import com.movisens.rxblemovisenssample.bluetooth.BluetoothService.Companion.COMMAND
+import com.movisens.rxblemovisenssample.bluetooth.BluetoothService.Companion.COMMAND_START
+import com.movisens.rxblemovisenssample.bluetooth.BluetoothService.Companion.SENSOR_MAC
 import com.polidea.rxandroidble2.exceptions.BleException
 import io.reactivex.exceptions.UndeliverableException
 import io.reactivex.plugins.RxJavaPlugins
@@ -20,6 +26,8 @@ class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
         // ignore BleExceptions as they were surely delivered at least once
         // Please read https://github.com/Polidea/RxAndroidBle/wiki/FAQ:-UndeliverableException
         RxJavaPlugins.setErrorHandler { error ->
@@ -29,14 +37,27 @@ class Application : Application() {
                 throw Exception(error)
             }
         }
-
+        val mac = sharedPreferences.getString(SENSOR_MAC, "") ?: ""
         if (isSamplingRunning()) {
-            val intent = Intent(this, BluetoothService::class.java)
-            if (SDK_INT > Build.VERSION_CODES.O) {
-                startForegroundService(intent)
+            if (mac.isNotEmpty()) {
+                val intent = Intent(this, BluetoothService::class.java)
+                intent.putExtra(SENSOR_MAC, mac)
+                intent.putExtra(COMMAND, COMMAND_START)
+
+                if (SDK_INT > O) {
+                    startForegroundService(intent)
+                } else {
+                    startService(intent)
+                }
             } else {
-                startActivity(intent)
+                sharedPreferences.edit {
+                    putBoolean("SAMPLING_RUNNING", false)
+                }
             }
+        }
+
+        createNotificationChannel()
+    }
 
     private fun createNotificationChannel() {
         if (SDK_INT >= O) {
